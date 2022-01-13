7c986f4a29ae5a67ecdf22b24e20f30f896b8a8f
Alex <alex.guillemin@edu.devinci.fr>
2022-01-13 14:32:21 +0100

    mobile version done

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector('.menu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+menu.addEventListener('click', close);
+
+function show(){
+    menu.style.display = 'flex';
+    menu.style.top = '0';
+}
+function close(){
+    menu.style.top = '-100%';
+}
